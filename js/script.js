0738f88be3e07fd175f0c61b70ea9a7b0a713e16
Marek Molinski <chbevi95@gmail.com>
2022-04-06 17:49:09 +0200

    test geo

--- js/script.js
+++ js/script.js
@@ -1,43 +1,48 @@
+
 const container = document.querySelector(".container#map")
 
 function geoFindMe() {
 
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      var mapOptions = {
-        center: [latitude, longitude],
-        zoom: 15
-     }
-     
-     // Creating a map object
-     var map = new L.map('map', mapOptions);
-     
-     // Creating a Layer object
-     var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-     
-     // Adding layer to the map
-     map.addLayer(layer);
-      // container.innerHTML = output
+        const status = document.querySelector('#status');
+        const mapLink = document.querySelector('#map-link');
+      
+        mapLink.href = '';
+        mapLink.textContent = '';
+      
+        function success(position) {
+          const status = document.querySelector('#status');
+          const mapLink = document.querySelector('#map-link');
+      
+          mapLink.href = '';
+          mapLink.textContent = '';
+          const latitude  = position.coords.latitude;
+          const longitude = position.coords.longitude;
 
+          var mapOptions = {
+          center: [latitude, longitude],
+          zoom: 15
+      }
+      
+      // Creating a map object
+      var map = new L.map('map', mapOptions);
+      
+      // Creating a Layer object
+      var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+      
+      // Adding layer to the map
+      map.addLayer(layer);
+          // container.innerHTML = output
     }
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if(!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
+
+      function error() {
+          status.textContent = 'Unable to retrieve your location';
+      }
+
+      if(!navigator.geolocation) {
+          status.textContent = 'Geolocation is not supported by your browser';
+      } else {
+          navigator.geolocation.getCurrentPosition(success, error);
+      }
   
   }
 
